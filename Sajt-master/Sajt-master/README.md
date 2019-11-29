6dd2a72abb4371392203cf7195d361081388928f
Nikola Jeremic <jeremicnikola97@gmail.com>
2019-11-29 16:21:06 +0100

    Preuredjeni folderi i izbrisan deo o nama!

--- Sajt-master/Sajt-master/README.md
+++
@@ -1,2 +0,0 @@
-# Sajt
-Ajde Bane
--- galerija.js
+++
@@ -1,7 +0,0 @@
-function otvori(imgs) {
-  var expandImg = document.getElementById("expandedImg");
-  var imgText = document.getElementById("imgtext");
-  expandImg.src = imgs.src;
-  imgText.innerHTML = imgs.title;
-  expandImg.parentElement.style.display = "block";
-}
